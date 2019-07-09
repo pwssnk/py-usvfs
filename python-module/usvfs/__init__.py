8fb7359ae1805c68501d27db530289956dd25d0e
pwssnk <pwssnk@live.nl>
2019-07-09 13:43:38 +0200

    Fix missing class export
    This is embarassing

--- python-module/usvfs/__init__.py
+++ python-module/usvfs/__init__.py
@@ -4,22 +4,23 @@ Python bindings for the userspace virtual filesystem (usvfs) library.
 """
 
 
-from .usvfs_wrapper import USVFSException, VirtualFile, VirtualDirectory, UserspaceVFS, dll
+from .usvfs_wrapper import USVFSException, VirtualFile, VirtualDirectory, Mapping, UserspaceVFS, dll
 
 __all__ = (
     'dll',
     'USVFSException',
     'VirtualDirectory',
     'VirtualFile',
+    'Mapping',
     'UserspaceVFS'
 )
 
 
-__author__ = "pwssnk"
-__copyright__ = "Copyright 2019, pwssnk"
-__credits__ = ["pwssnk", "Usvfs developers"]
-__license__ = "GPLv3"
-__version__ = "0.1.0"
-__maintainer__ = "pwssnk"
-__email__ = ""
-__status__ = "Development"
+__author__ = 'pwssnk'
+__copyright__ = 'Copyright 2019, pwssnk'
+__credits__ = ['pwssnk', 'Usvfs developers']
+__license__ = 'GPLv3'
+__version__ = '0.1.1'
+__maintainer__ = 'pwssnk'
+__email__ = ''
+__status__ = 'Development'
